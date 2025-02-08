036f10bc754853e72a9ec9a27ba8dbb31eab5478
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-02-08 17:29:24 -0300

    adiciona class raizQuadradaDeVinteECinco

--- src/com/betrybe/helloworld/HelloWorld.java
+++ src/com/betrybe/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
+//HelloWorld.java
+package com.betrybe.helloworld;
 
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Primeira Linha");
         System.out.println("Segunda Linha");
---
+++ src/com/betrybe/helloworld/RaizQuadradaDeVinteECinco.java
@@ -0,0 +1,14 @@
+//RaizQuadradaDeVinteECinco.java
+package com.betrybe.helloworld;
+
+import java.lang.*;
+import java.lang.Math;
+
+public class RaizQuadradaDeVinteECinco {
+    public static void main(String[] args) {
+        System.out.println("A raiz quadrada de vinte e cinco é:");
+        System.out.println(
+                Math.sqrt(25)
+        );
+    }
+}
