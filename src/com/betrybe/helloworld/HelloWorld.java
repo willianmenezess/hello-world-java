54fa5c3ecc38a5e9866cdd2f48b2dc3b6c2e6857
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-02-27 21:29:55 -0300

    coments

--- src/com/betrybe/helloworld/HelloWorld.java
+++ src/com/betrybe/helloworld/HelloWorld.java
@@ -1,6 +1,7 @@
 //HelloWorld.java
 package com.betrybe.helloworld;
 
+// A não ser que você esteja escrevendo uma biblioteca, seus programas sempre terão um main.
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Primeira Linha");
