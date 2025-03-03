74ddee0a13304da948fc698d14c3c26dd57abd0a
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-03-03 17:47:21 -0300

    exercicio Residente.java

--- src/com/betrybe/exercicios/Residente.java
+++ src/com/betrybe/exercicios/Residente.java
@@ -1,26 +1,35 @@
 package com.betrybe.exercicios;
+//importando o Math para usar o método pow
+import java.lang.Math;
+
 
 public class Residente {
-    // Atributos demais
+    // Atributos
     public String nome;
     public int idade;
     public double peso;
     public double altura;
+    public double imc;
 
     // Metodo Construtor
-    public Residente (String nome, int idade, double peso, double altura) {
+    public Residente(String nome, int idade, double peso, double altura) {
         this.nome = nome;
         this.idade = idade;
         this.peso = peso;
         this.altura = altura;
     }
 
-    public static void Main(String[] args) {
+    public double calcularImc() {
+        return this.peso / Math.pow(this.altura, 2);
+    }
+
+    public static void main(String[] args) {
         Residente residente = new Residente("João", 25, 70.0, 1.75);
         System.out.println(residente.nome); // João
         System.out.println(residente.idade); // 25
         System.out.println(residente.peso); // 70.0
         System.out.println(residente.altura); // 1.75
+        System.out.println("O IMC de " + residente.nome + " é: " + residente.calcularImc()); // O IMC de João é: 22.857142857142858
     }
 }
 
