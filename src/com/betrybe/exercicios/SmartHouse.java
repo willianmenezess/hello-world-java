60ad0f7a97e0707c9249f2515e897cf4f5644475
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-03-03 07:43:01 -0300

    exercicio SmartHouse

---
+++ src/com/betrybe/exercicios/SmartHouse.java
@@ -0,0 +1,40 @@
+package com.betrybe.exercicios;
+
+/**
+ * Classe.
+ **/
+public class SmartHouse {
+
+    /**
+     * Atributos.
+     */
+    public boolean lampadaLigada;
+
+    /**
+     * Construtor da classe SmartHouse.
+     */
+    public SmartHouse() {
+        this.lampadaLigada = false;
+    }
+
+    /**
+     * Método para ligar uma lâmpada.
+     */
+    public void ligarLampada() {
+        this.lampadaLigada = true;
+    }
+
+    /**
+     * Método para desligar uma lâmpada.
+     */
+    public void desligarLampada() {
+        this.lampadaLigada = false;
+    }
+
+    /**
+     * Método para verificar se uma lâmpada está ligada ou não.
+     */
+    public boolean isLampadaLigada() {
+        return lampadaLigada;
+    }
+}
